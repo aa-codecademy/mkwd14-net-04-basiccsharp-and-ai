3839491629f7c7541d5722d6c1043da26512bd06
Martin Panovski <panovski.martin93@gmail.com>
2026-03-18 20:51:39 +0100

    Class 3 demo code part 2 added

--- G1/Class03/code/Class03/Class03.Arrays/Program.cs
+++ G1/Class03/code/Class03/Class03.Arrays/Program.cs
@@ -1,7 +1,12 @@
-﻿#region Arrays
+﻿using System.Collections;
+
+
+
+
+#region Arrays
 
 // Declaration of an array in C#
-using System.Collections;
+
 
 int[] emptyNumbersArray = new int[3];
 
@@ -48,5 +53,75 @@ var mixedArray = new int[] { 1, 23, 34, 5 };
 // AVOID and DON't use this unless you need to do so for some reason!!!
 var mixedArrayValues = new object[] { 1, 1.1, true, "Martin" };
 ArrayList mixed = new ArrayList { 1, 2.4, true, false };
+
+#endregion
+
+#region Array Methods
+
+int[] evenNumbers = new int[] { 2, 4, 6, 8, 10, 2, 4 };
+
+// Reverse the array elements
+//Array.Reverse(evenNumbers);
+
+Console.WriteLine("Array of even numbers after Reverse()");
+foreach (var num in evenNumbers)
+{
+    Console.WriteLine(num);
+}
+
+
+int indexFound = Array.IndexOf(evenNumbers, 2);
+Console.WriteLine("The index of number 2 is " + indexFound);
+#endregion
+
+
+#region Array Resize method using example use case
+
+string[] usernames = new string[0];
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Please enter a username:");
+    string userInput = Console.ReadLine();
+
+    Array.Resize(ref usernames, usernames.Length + 1);
+    usernames[usernames.Length - 1] = userInput;
+
+    Console.WriteLine("Do you want to insert new username? Y/N");
+    userInput = Console.ReadLine();
+
+    if (userInput == "n" || userInput == "N")
+        break;
+
+
+} while (true);
+
+foreach (var username in usernames)
+{
+    Console.WriteLine(username);
+}
+
+
+
+int[] test = new int[] { 1, 2, 3, 4, 5 };
+
+Array.Resize(ref test, 10);
+
+test[5] = 10;
+test[6] = 25;
+test[7] = 25;
+test[8] = 25;
+
+
+
+int[][] arrayOfArrays = new int[][] 
+{
+    new int[] {1, 2},
+    new int[] {1, 2},
+    new int[] {1, 2},
+};
+
+
 
 #endregion
