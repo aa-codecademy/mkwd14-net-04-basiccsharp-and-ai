5a995ed15ef1211dad7e17de106839e2fdae57f1
Martin Panovski <panovski.martin93@gmail.com>
2026-03-18 20:01:26 +0100

    Class 03 demo code part 1 added

---
+++ G1/Class03/code/Class03/Class03.Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Arrays
+
+// Declaration of an array in C#
+using System.Collections;
+
+int[] emptyNumbersArray = new int[3];
+
+// Initialization of array elements
+emptyNumbersArray[0] = 1;
+emptyNumbersArray[1] = 10;
+emptyNumbersArray[2] = 20;
+
+// Cannot add new element on non existing index
+// This will throw IndexOutOfRangeException
+//emptyNumbersArray[3] = 50;
+
+for (int i = 0; i < emptyNumbersArray.Length; i++)
+{
+    Console.WriteLine(emptyNumbersArray[i]);
+}
+
+string[] names = new string[]
+{
+    "Martin",
+    "Mario",
+    "Dimitar",
+    "Ognen",
+    "Damjan"
+};
+
+double[] decimalNumbers = [10.5, 20.4, 100.12];
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+foreach (double number in decimalNumbers)
+{
+    Console.WriteLine(number);
+}
+
+
+var mixedArray = new int[] { 1, 23, 34, 5 };
+
+
+// Ways of declaration and use of arrays with mixed types in it.
+// AVOID and DON't use this unless you need to do so for some reason!!!
+var mixedArrayValues = new object[] { 1, 1.1, true, "Martin" };
+ArrayList mixed = new ArrayList { 1, 2.4, true, false };
+
+#endregion
---
+++ G1/Class03/code/Class03/Class03.Exercises/Program.cs
@@ -0,0 +1,62 @@
+﻿#region Exercise 1
+Console.Write("Enter an integer: ");
+int input = int.Parse(Console.ReadLine());
+
+
+// 1) Print all numbers from 1 up to that number
+Console.Write("1 up to " + input + ": ");
+for (int i = 1; i <= input; i++)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+// 2) Print all numbers starting from that number down to 1
+Console.Write(input + " down to 1: ");
+for (int i = input; i >= 1; i--)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+// 3) Print all even numbers starting from 2
+Console.Write("Even numbers (starting from 2): ");
+for (int i = 2; i <= input; i += 2)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+// 4) Print all odd numbers starting from 1
+Console.Write("Odd numbers (starting from 1): ");
+for (int i = 1; i <= input; i += 2)
+{
+    Console.Write(i + " ");
+}
+#endregion
+
+Console.ReadLine();
+
+#region Exercise 2
+Console.Write("Enter an integer: ");
+int userInput = int.Parse(Console.ReadLine());
+
+int j = 1;
+while (j <= userInput)
+{
+    if (j == 100)
+    {
+        Console.WriteLine("The limit is reached");
+        break;
+    }
+
+    if (j % 3 == 0 || j % 7 == 0)
+    {
+        j++;
+        continue;
+    }
+
+    Console.Write(j + " ");
+    j++;
+}
+#endregion
