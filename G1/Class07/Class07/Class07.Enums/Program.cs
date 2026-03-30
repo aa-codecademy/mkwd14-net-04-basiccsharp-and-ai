5fa29808af36a94f1da88d7ecc672e1492e852d4
Martin Panovski <panovski.martin93@gmail.com>
2026-03-30 20:45:50 +0200

    Class 07 demo code added

---
+++ G1/Class07/Class07/Class07.Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿using Class07.Enums.Enums;
+using Class07.MathLibrary;
+using System.Data.Common;
+
+Console.WriteLine(DaysOfWeek.Monday);
+Console.WriteLine(Convert.ToInt32(DaysOfWeek.Monday));
+
+
+Console.WriteLine("Please enter day of week:");
+string input = Console.ReadLine();
+
+// Bad way! Error prone!
+//if(input == "Satrday" || input == "Sunday")
+//{
+//    Console.WriteLine("Yeey! It is a weekend!");
+//}
+
+// Better way! 
+if (input == DaysOfWeek.Saturday.ToString() || input == DaysOfWeek.Sunday.ToString())
+{
+    Console.WriteLine("Yeey! It is a weekend!");
+}
+
+
+MathOperations.Sum(10, 20);
+
+Cat cat = new Cat();
---
+++ G1/Class07/Class07/Class07.Inheritance/Models/Parrot.cs
@@ -0,0 +1,28 @@
+﻿namespace Class07.Inheritance.Models;
+
+public class Parrot : Animal
+{
+    public string Color { get; set; }
+
+    public Parrot()
+    {
+        
+    }
+
+    public Parrot(int id, string name, string type, string color)
+        : base(id, name, type)
+    {
+        Color = color;
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Wooow! I am flying!");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine($"I am {Name} and I am {Type} who don't have time to eat, because I speak all the time!");
+    }
+
+}
