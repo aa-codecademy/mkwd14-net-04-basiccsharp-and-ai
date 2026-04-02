183f29f6d91f0dc5f045e6b7493f82c853d9785e
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2026-04-02 20:59:59 +0200

    Class 07

--- G3/Class07/Class07/Constructors/Program.cs
+++ G3/Class07/Class07/Constructors/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello, Constructors!");
+﻿using Constructors;
 
+Console.WriteLine("Hello, Constructors!");
+
+
+Console.WriteLine("\n======= Default Constructor =======");
+
+User john = new User();
+john.Name = "John Doe";
+john.PrintInfo();
+
+
+Console.WriteLine("\n======= Parameterized Constructors =======");
+
+User jane = new User("Jane Doe", 30);
+jane.PrintInfo();
+
+User bob = new User("Bob Bobsky", 34, "bob@bobsky.com", Role.Admin);
+bob.PrintInfo();
+
+
+Console.ReadLine();
---
+++ G3/Class07/Class07/Domain/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public Seniority Seniority { get; set; }
+
+        // TIP: Prefer vertical style of coding when having multiple parameters to improve readability
+        public Employee(
+            int id,
+            string firstName,
+            string lastName,
+            int age,
+            double salary,
+            Seniority seniority)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+            Seniority = seniority;
+        }
+
+        // Virtual methods - can be overridden by derived classes
+        public virtual string GetDetails()
+        {
+            return $"[{Id}] {FirstName} {LastName}, Age: {Age}, Salary: {Salary:C}, Seniority: {Seniority}";
+        }
+
+        public virtual double CalculateAnnualBonus()
+        {
+            return Salary * 0.1;
+        }
+
+    }
+}
